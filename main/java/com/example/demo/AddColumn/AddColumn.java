30abe0331a0643e78a732fbbbc6258fa514a52cf
Kartiki Sanap <sanapkk123@gmail.com>
2022-03-27 16:43:41 +0530

    comment added

--- main/java/com/example/demo/AddColumn/AddColumn.java
+++ main/java/com/example/demo/AddColumn/AddColumn.java
@@ -2,7 +2,8 @@ package com.example.demo.AddColumn;
 
 import javax.sql.DataSource;
 
-public interface AddColumn {
+//interface for ading column 
+ublic interface AddColumn {
 
 	public boolean AddNewColumn(DataSource datasource, String project, String table, String column)throws Exception;
 	public boolean AddAllColumns(DataSource datasource, String project, String table)throws Exception;
